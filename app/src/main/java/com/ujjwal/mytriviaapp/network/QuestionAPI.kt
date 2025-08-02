261cbc99a75a08d8b27e5dea79e8fa5c88d88d48
UjjawalKaul <ujjawal@vobble.fun>
2025-08-03 00:25:28 +0530

    Added QuestionAPI using Retrofit and base url in constants

---
+++ app/src/main/java/com/ujjwal/mytriviaapp/network/QuestionAPI.kt
@@ -0,0 +1,11 @@
+package com.ujjwal.mytriviaapp.network
+
+import com.ujjwal.mytriviaapp.model.Question
+import retrofit2.http.GET
+import javax.inject.Singleton
+
+@Singleton
+interface QuestionAPI {
+    @GET("world.json")
+    suspend fun getAllQuestions():Question
+}
