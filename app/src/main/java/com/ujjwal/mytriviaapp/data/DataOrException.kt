8a4b47a5fa746f30e7fd089a3ac9588000465687
UjjawalKaul <ujjawal@vobble.fun>
2025-08-03 01:17:48 +0530

    Added GetQuestions repo function

--- app/src/main/java/com/ujjwal/mytriviaapp/data/DataOrException.kt
+++ app/src/main/java/com/ujjwal/mytriviaapp/data/DataOrException.kt
@@ -2,6 +2,6 @@ package com.ujjwal.mytriviaapp.data
 
 data class DataOrException<T,Boolean,E:Exception>(
     var data:T?=null,
-    val loading:Boolean?=null,
+    var loading:Boolean?=null,
     var e:E?=null
 )
--- app/src/main/java/com/ujjwal/mytriviaapp/repository/QuestionRepository.kt
+++ app/src/main/java/com/ujjwal/mytriviaapp/repository/QuestionRepository.kt
@@ -1,13 +1,27 @@
 package com.ujjwal.mytriviaapp.repository
 
+import android.util.Log
 import com.ujjwal.mytriviaapp.data.DataOrException
 import com.ujjwal.mytriviaapp.model.Question
 import com.ujjwal.mytriviaapp.network.QuestionAPI
 import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(
-    private val api:QuestionAPI
+    private val api: QuestionAPI
 ) {
-    private val listOfQuestions=DataOrException<ArrayList<Question>,Boolean,Exception>()
+    private val TAG = "QuestionRepository"
+    private val dataOrException = DataOrException<Question, Boolean, Exception>()
 
+    suspend fun getAllQuestion(): DataOrException<Question, Boolean, Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+
+        } catch (e: Exception) {
+            dataOrException.e = e
+            Log.d(TAG, "Exception Occurred: ${e.localizedMessage} ")
+        }
+        return dataOrException
+    }
 }
