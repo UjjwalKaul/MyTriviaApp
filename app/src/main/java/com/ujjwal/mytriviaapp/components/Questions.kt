5849dcc01fb7933112cb762ca159a7b04b4cf290
UjjawalKaul <ujjawal@vobble.fun>
2025-08-03 13:41:08 +0530

    Drawline using Canvas

--- app/src/main/java/com/ujjwal/mytriviaapp/components/Questions.kt
+++ app/src/main/java/com/ujjwal/mytriviaapp/components/Questions.kt
@@ -1,9 +1,12 @@
 package com.ujjwal.mytriviaapp.components
 
 import android.util.Log
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
@@ -11,11 +14,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,6 +47,7 @@ fun Questions(viewModel: QuestionsViewModel = hiltViewModel()) {
 @Preview
 @Composable
 fun QuestionDisplay() {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
     Surface(
         modifier = Modifier
             .fillMaxSize()
@@ -55,6 +59,7 @@ fun QuestionDisplay() {
             horizontalAlignment = Alignment.Start
         ) {
             QuestionTracker()
+            DottedLine(pathEffect)
         }
 
     }
@@ -86,4 +91,21 @@ fun QuestionTracker(counter: Int = 10, outOff: Int = 100) {
         },
         modifier = Modifier.padding(20.dp)
     )
+}
+
+@Preview
+@Composable
+fun DottedLine(pathEffect: PathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(2.dp)
+    ) {
+        drawLine(
+            color = AppColors.mTrueWhite,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            pathEffect = pathEffect
+        )
+    }
 }
